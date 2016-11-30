a447397e2304ad77be22204db35385411da1846f
malmax <malmax.spb@gmail.com>
2016-11-30 17:59:12 +0300

    init hw

--- homeworks/lesson02hw/src/lesson02hw.js
+++ homeworks/lesson02hw/src/lesson02hw.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Main from './app/components/main.component.jsx';
+import Clock from './app/components/clock.component.jsx';
 
 let app = document.getElementById('app');
 //debugger;
